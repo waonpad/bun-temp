ec4899d165b950f3359abf81b767395da8ad5944
waonpad <waonpad@gmail.com>
2024-10-29 20:43:02 +0900

    chore: Update npm dependencies and configuration files

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,14 @@
-import path from 'node:path';
-import { defineConfig } from 'vitest/config';
+import { resolve } from "node:path";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
   test: {
     globals: false,
-    // environment: 'happy-dom',
-    include: ['src/**/*.test.{js,ts,jsx,tsx}'],
+    include: ["src/**/*.test.ts"],
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
     setupFiles: [],
   },
